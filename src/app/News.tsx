6772801ac9249eb1495a6d86488d7da201d2ff09
Johan Sandoval <jojo.han718@gmail.com>
2024-03-31 09:55:55 -0500

    Image bug fix

--- src/app/News.tsx
+++ src/app/News.tsx
@@ -4,7 +4,7 @@ export default function News() {
   return (
     <div className="h-full w-full flex flex-col sm:overflow-auto items-center gap-4 pb-4 justify-center">
       <h1 className="text-xl lg:text-3xl mx-8 sm:mx-16 lg:mx-32 text-red-700 font-bold text-center p-2">Basket of Strawberries at the Renegade Fair May18th and 19th! (hopefully 🤞🥹)</h1>
-      <img src="Renegade Vintage.png" className="object-cover rounded-lg h-auto w-9/12"></img>
+      <img src="Renegade.png" className="object-cover rounded-lg h-auto w-9/12"></img>
     </div>
   );
 }

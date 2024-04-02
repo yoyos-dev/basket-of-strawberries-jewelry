2643e200465ef3d1e30cd15b66020f64ba67ad56
Johan Sandoval <jojo.han718@gmail.com>
2024-04-02 15:44:10 -0500

    Bug fix with height on Booth Concept and News pgs

--- src/app/Booth Concept.tsx
+++ src/app/Booth Concept.tsx
@@ -2,7 +2,7 @@
 
 export default function BoothConcept() {
   return (
-    <div className="h-full w-full flex flex-col sm:overflow-auto items-center gap-4 pb-4 justify-center">
+    <div className="h-full w-full flex flex-col sm:overflow-auto items-center gap-4 pb-4">
       <h1 className="text-3xl mx-8 sm:mx-16 lg:mx-32 text-red-700 font-bold text-center p-2">Booth Concept</h1>
       <img src="Booth Concept.png" className="object-cover h-auto w-3/4 lg:w-7/12 mb-44 sm:mb-0"></img>
     </div>

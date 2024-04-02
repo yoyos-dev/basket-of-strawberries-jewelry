3c013393a1ea95ed590ccaba4479ec93cd45e0d1
Johan Sandoval <jojo.han718@gmail.com>
2024-04-02 15:01:30 -0500

    Edit metadata and social media buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,24 @@ import JewelryBox from "./JewelryBox";
 import News from "./News";
 import About from "./About";
 import BoothConcept from "./Booth Concept";
-import InteractiveButton from './InteractiveButton';
 import React, { useState, useEffect } from 'react';
 
 export default function Home() {
   const [selectedComponent, setSelectedComponent] = useState('JewelryBox');
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isSocialsHovered, setIsSocialsHovered] = useState(false);
+  
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 640) {
+        exitNav();
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -30,17 +42,23 @@ export default function Home() {
       sideNav.style.width = "60%";
     }
     setIsNavOpen(true);
-  }
+  };
 
   function exitNav() {
-  const sideBar = document.getElementById("sideBar");
-  const sideNav = document.getElementById("sideNav");
-  if (sideBar && sideNav) {
-    sideBar.style.width = "0";
-    sideNav.style.width = "0";
-  }
-  setIsNavOpen(false);
-}
+    const sideBar = document.getElementById("sideBar");
+    const sideNav = document.getElementById("sideNav");
+    if (sideBar && sideNav && !isSocialsHovered) {
+      sideBar.style.width = "0";
+      sideNav.style.width = "0";
+    }
+    if (!isSocialsHovered) {
+      setIsNavOpen(false);
+    }
+  };
+
+  const openInNewTab = (url: string) => {
+    window.open(url, "_blank", "noreferrer");
+  };
 
   return (
     <main className="flex flex-col h-screen w-screen">
@@ -71,22 +89,14 @@ export default function Home() {
           </svg>
         </div>
 
-        <div id="sideBar">
+        <div onClick={exitNav} id="sideBar">
           <div id="sideNav" className={`fixed top-0 left-0 bg-slate-300 z-10 overflow-x-hidden duration-200 font-bold flex flex-col justify-center items-center h-full ${isNavOpen ? 'w-60' : 'w-0'}`}>
             <a onClick={exitNav} className="text-black text-3xl absolute top-0 left-0 ml-2 mt-1 hover:cursor-pointer select-none">&times;</a>
 
-            <p className="text-red-700">Basket of Strawberries</p>
+            <p className="text-red-700 bg-rose-300 p-4 mb-10 w-full">Basket of Strawberries</p>
 
-            <div className="flex flex-row gap-2 justify-center my-6">
-              <InteractiveButton url="https://www.facebook.com/profile.php?id=61557517305356&mibextid=LQQJ4d" imgSrc="Icons/Facebook.png" />
-              <InteractiveButton url="https://www.instagram.com/basketofstrawberry/" imgSrc="Icons/Instagram.png" />
-              <InteractiveButton url="https://www.tiktok.com/@basketofstrawberry" imgSrc="Icons/TikTok.png" />
-              <a href="mailto:Basketofstrawberryjewelry@gmail.com" className="rounded-full h-12 w-12">
-                  <img src={"Icons/Gmail.png"} alt="" className="fill-red-700"/>
-              </a>
-            </div>
 
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-1">
               <button className={`text-red-700 text-2xl text-center ${selectedComponent === 'JewelryBox' ? 'underline' : ''}`}
   onClick={() => setSelectedComponent('JewelryBox')}>Portfolio</button>
               <button className={`text-red-700 text-2xl text-center ${selectedComponent === 'Booth Concept' ? 'underline' : ''}`}
@@ -96,6 +106,20 @@ export default function Home() {
               <button className={`text-red-700 text-2xl text-center ${selectedComponent === 'News' ? 'underline' : ''}`}
   onClick={() => setSelectedComponent('News')}>News</button>
             </div>
+
+            <div onMouseEnter={() => setIsSocialsHovered(true)} onMouseLeave={() => setIsSocialsHovered(false)} className="group flex flex-col mt-12 relative mx-auto h-32 justify-end">
+              <a role="link" onClick={() => openInNewTab("https://www.facebook.com/profile.php?id=61557517305356&mibextid=LQQJ4d")}>
+                <img src="Icons/Facebook Strawberry.png" className="mx-auto absolute w-1/4 z-0 left-[15%] bottom-0 -rotate-12 group-hover:w-1/3 group-hover:bottom-20 group-hover:left-[10%] duration-200 group-hover:animate-hoverA hover:cursor-pointer"></img>
+              </a>
+              <a role="link" onClick={() => openInNewTab("https://www.instagram.com/basketofstrawberry/")}>
+              <img src="Icons/Instagram Strawberry.png" className="mx-auto absolute w-1/4 z-20 left-[38%] bottom-0 rotate-12 group-hover:w-1/3 group-hover:bottom-20 group-hover:left-[35%] duration-200 group-hover:animate-hoverB hover:cursor-pointer"></img>
+              </a>
+              <a role="link" onClick={() => openInNewTab("https://www.tiktok.com/@basketofstrawberry")}>
+              <img src="Icons/TikTok Strawberry.png" className="mx-auto absolute w-1/4 z-10 left-[58%] bottom-0 -rotate-12 group-hover:w-1/3 group-hover:bottom-20 group-hover:left-[60%] duration-200 group-hover:animate-hoverC hover:cursor-pointer"></img>
+              </a>
+              <img src="Strawberry Basket.png" className="mx-auto w-10/12 z-40 group-hover:w-1/2 duration-200"></img>
+            </div>
+
           </div>
         </div>
 
@@ -106,22 +130,13 @@ export default function Home() {
       <div className="flex flex-col sm:flex-row w-full flex-grow sm:overflow-hidden">
         {/* Profile Bar */}
         <div className="flex flex-col h-full w-full sm:w-1/2 xl:w-1/4 items-center overflow-auto">
-          <img src="Profile.jpg" className="rounded-2xl shadow-xl h-60 w-52 my-8 object-cover"></img>
+          <img src="Profile.jpg" className="rounded-2xl shadow-xl h-60 w-52 my-6 object-cover hover:cursor-help"></img>
           <p className="text-red-700 text-3xl text-center">Jazmin Gong</p>
-          <p className="text-black text-2xl text-center my-2">Chicago, IL</p>
-          <p className="text-gray-500 text-sm text-center mx-2 my-2">Like a grocery store but Jewelry. Handmade and original designs specializing in necklaces and earrings. :)</p>
+          <p className="text-black text-2xl text-center">Chicago, IL</p>
+          <p className="text-gray-500 text-sm text-center mx-2 my-1">Like a grocery store but Jewelry. Handmade and original designs specializing in necklaces and earrings. :)</p>
 
         <div className="hidden sm:block">
-            <div className="flex flex-row gap-2 justify-center my-6">
-              <InteractiveButton url="https://www.facebook.com/profile.php?id=61557517305356&mibextid=LQQJ4d" imgSrc="Icons/Facebook.png" />
-              <InteractiveButton url="https://www.instagram.com/basketofstrawberry/" imgSrc="Icons/Instagram.png" />
-              <InteractiveButton url="https://www.tiktok.com/@basketofstrawberry" imgSrc="Icons/TikTok.png" />
-              <a href="mailto:Basketofstrawberryjewelry@gmail.com" className="rounded-full h-12 w-12">
-                  <img src={"Icons/Gmail.png"} alt="" className="fill-red-700"/>
-              </a>
-            </div>
-
-            <div className="flex flex-col gap-2 my-10">
+            <div className="flex flex-col gap-2 my-2">
               <button className={`text-red-700 text-2xl text-center ${selectedComponent === 'JewelryBox' ? 'underline' : ''}`}
   onClick={() => setSelectedComponent('JewelryBox')}>Portfolio</button>
               <button className={`text-red-700 text-2xl text-center ${selectedComponent === 'Booth Concept' ? 'underline' : ''}`}
@@ -131,6 +146,21 @@ export default function Home() {
               <button className={`text-red-700 text-2xl text-center ${selectedComponent === 'News' ? 'underline' : ''}`}
   onClick={() => setSelectedComponent('News')}>News</button>
             </div>
+
+            {/*Basket of Strawberries Socials */}
+            <div className="group flex flex-col mt-12 relative mx-auto h-32 justify-end">
+              <a role="link" onClick={() => openInNewTab("https://www.facebook.com/profile.php?id=61557517305356&mibextid=LQQJ4d")}>
+                <img src="Icons/Facebook Strawberry.png" className="mx-auto absolute sm:w-1/5 xl:w-1/4 z-0 sm:left-[22%] xl:left-[15%] bottom-0 -rotate-12 sm:group-hover:w-1/4 xl:group-hover:w-1/3 group-hover:bottom-20 sm:group-hover:left-[18%] xl:group-hover:left-[10%] duration-200 group-hover:animate-hoverA hover:cursor-pointer"></img>
+              </a>
+              <a role="link" onClick={() => openInNewTab("https://www.instagram.com/basketofstrawberry/")}>
+              <img src="Icons/Instagram Strawberry.png" className="mx-auto absolute sm:w-1/5 xl:w-1/4 z-20 sm:left-[40%] xl:left-[38%] bottom-0 rotate-12 sm:group-hover:w-1/4 xl:group-hover:w-1/3 group-hover:bottom-20 sm:group-hover:left-[38%] xl:group-hover:left-[35%] duration-200 group-hover:animate-hoverB hover:cursor-pointer"></img>
+              </a>
+              <a role="link" onClick={() => openInNewTab("https://www.tiktok.com/@basketofstrawberry")}>
+              <img src="Icons/TikTok Strawberry.png" className="mx-auto absolute sm:w-1/5 xl:w-1/4 z-10 sm:left-[57%] xl:left-[58%] bottom-0 -rotate-12 sm:group-hover:w-1/4 xl:group-hover:w-1/3 group-hover:bottom-20 sm:group-hover:left-[58%] xl:group-hover:left-[60%] duration-200 group-hover:animate-hoverC hover:cursor-pointer"></img>
+              </a>
+              <img src="Strawberry Basket.png" className="mx-auto sm:w-8/12 xl:w-10/12 z-40 sm:group-hover:w-5/12 xl:group-hover:w-1/2 duration-200"></img>
+            </div>
+
           </div>
         </div>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,28 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      keyframes: {
+        hoverA: {
+          '0%, 100%': { transform: 'translate(0%,-10%) rotate(-12deg)' },
+          '50%': { transform: 'translate(0%,0%) rotate(-10deg)' },
+        },
+        hoverB: {
+          '0%, 100%': { transform: 'translate(0%,10%) rotate(12deg)' },
+          '50%': { transform: 'translate(0%,0%) rotate(10deg)' },
+        },
+        hoverC: {
+          '0%, 100%': { transform: 'translate(0%,-8%) rotate(13deg)' },
+          '50%': { transform: 'translate(0%,2%) rotate(8deg)' },
+        },
+      },
+      animation: {
+        hoverA: 'hoverA 5s ease-in-out infinite',
+        hoverB: 'hoverB 5s ease-in-out infinite',
+        hoverC: 'hoverC 5s ease-in-out infinite',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
